b387177cc08467fbec8c46b04087f16ba241197d
root <root@fgb.microchip.com>
2021-02-08 17:54:02 -0800

    up

---
+++ matrix.X/build/default/production/_ext/1472/swm45k80.p1.d
@@ -0,0 +1,12 @@
+build/default/production/_ext/1472/swm45k80.p1:  \
+../swm45k80.c  \
+../matrix.X/mcc_generated_files/mcc.h  \
+../matrix.X/mcc_generated_files/device_config.h  \
+../matrix.X/mcc_generated_files/pin_manager.h  \
+../matrix.X/mcc_generated_files/interrupt_manager.h  \
+../matrix.X/mcc_generated_files/tmr1.h  \
+../matrix.X/mcc_generated_files/memory.h  \
+../matrix.X/mcc_generated_files/tmr2.h  \
+../matrix.X/mcc_generated_files/tmr0.h  \
+../matrix.X/mcc_generated_files/adc.h  \
+../matrix.X/mcc_generated_files/eusart2.h 
